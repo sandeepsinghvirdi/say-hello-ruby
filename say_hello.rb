05e6fbfb1ff6c4ea6cc6842122a13ea7831f894d
sandeepsinghvirdi <sandeep_sonu07@yahoo.com>
2022-07-29 15:57:15 +1000

    commit tree cce7b6a89acedd4831f52d840214550db592f460

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,11 @@
-# Build your say_hello method here
+#Build your say_hello method here
+#name = "Hello Ruby Programmer!"
+def say_hello(name)
+   puts "Hello #{name}!"
+end
+def say_hello
+puts "Hello Ruby Programmer!"
+end
+say_hello
+say_hello("Kent Beck")
+
